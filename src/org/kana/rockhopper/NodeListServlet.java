ec5424b5bdddb0d3b0d222847a69e8bdd8c8745f
giowork <giovanni.coia@verint.com>
2015-03-27 15:28:19 -0700

    adding ability to get individual node name

--- src/org/kana/rockhopper/NodeListServlet.java
+++ src/org/kana/rockhopper/NodeListServlet.java
@@ -17,6 +17,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.kana.rockhopper.chef.NodePojo;
 
+
+
 /**
  * Servlet implementation class NodeListServlet
  */
@@ -36,9 +38,98 @@ public class NodeListServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			JSONArray jsonArrayNodes = geNodeList();
+			returnJSONArrayOfNodes(request, response, jsonArrayNodes);
+		}
+		catch(Exception exp) {
+			exp.printStackTrace();
+		}
 	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf8");
+		response.setContentType("application/json");
 
+		if (request.getParameter("nodeName") != null) {
+			PrintWriter out = response.getWriter();
+			ChefClient cc = new ChefClient();
+			String jsonNode = cc.getNode(request.getParameter("nodeName"));
+			if (jsonNode != null) {
+				try {
+					JSONObject jo = getNodeDetails(jsonNode);
+					out.print(jo);
+				} 
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				response.sendError(400);
+			}
+		} 
+	}
+	
+private JSONObject getNodeDetails(String nodeJson) {
+		
+		ObjectMapper objMapper = new ObjectMapper();
+		JSONObject jsonObj = new JSONObject();
+		NodePojo nodePojo = new NodePojo();
+		JSONObject jsonObjForUI = new JSONObject();
+		
+		try {			
+			    jsonObj = new JSONObject(nodeJson);
+				nodePojo = new NodePojo();
+				
+				nodePojo.setName(jsonObj.getString("name"));
+				nodePojo.setChef_type(jsonObj.getString("chef_type"));
+				nodePojo.setChef_environment(jsonObj.getString("chef_environment"));
+				nodePojo.setNodeRunList(jsonObj.getJSONArray("run_list").toString());
+				
+				if(jsonObj.getJSONObject("automatic") != null && jsonObj.getJSONObject("automatic").length() > 0) {
+					if(jsonObj.getJSONObject("automatic").getString("uptime") != null) {
+						nodePojo.setUptime(jsonObj.getJSONObject("automatic").getString("uptime"));
+					}
+					else nodePojo.setUptime("");
+					
+					nodePojo.setHostname(jsonObj.getJSONObject("automatic").getString("hostname"));
+					nodePojo.setFqdn(jsonObj.getJSONObject("automatic").getString("fqdn"));
+					nodePojo.setOs_version(jsonObj.getJSONObject("automatic").getString("os_version"));
+					nodePojo.setPlatform_version(jsonObj.getJSONObject("automatic").getString("platform_version"));
+					nodePojo.setCpu_total(jsonObj.getJSONObject("automatic").getJSONObject("cpu").getInt("total"));
+					nodePojo.setPlatform(jsonObj.getJSONObject("automatic").getString("platform"));
+					nodePojo.setOs(jsonObj.getJSONObject("automatic").getString("os"));
+					nodePojo.setIpaddress(jsonObj.getJSONObject("automatic").getString("ipaddress"));
+					nodePojo.setRecipes(jsonObj.getJSONObject("automatic").getJSONArray("recipes").toString());
+				}
+				else {
+					nodePojo.setUptime("Node bootstrapped yet");
+					nodePojo.setHostname("Node bootstrapped yet");
+					nodePojo.setFqdn("Node bootstrapped yet");
+					nodePojo.setOs_version("Node bootstrapped yet");
+					nodePojo.setPlatform_version("Node bootstrapped yet");
+					nodePojo.setCpu_total(0);
+					nodePojo.setPlatform("Node bootstrapped yet");
+					nodePojo.setOs("Node bootstrapped yet");
+					nodePojo.setIpaddress("Node bootstrapped yet");
+					nodePojo.setRecipes("Node bootstrapped yet");
+					
+				}
+
+				jsonObjForUI = new JSONObject(objMapper.writer().withDefaultPrettyPrinter().writeValueAsString(nodePojo));
+			
+		}
+		catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return jsonObjForUI;
+	}
+	
 	/*
 	 * function to execute the command from command line.
 	 */
@@ -66,17 +157,36 @@ public class NodeListServlet extends HttpServlet {
 				nodePojo.setChef_type(jsonObj.getString("chef_type"));
 				nodePojo.setChef_environment(jsonObj.getString("chef_environment"));
 				nodePojo.setNodeRunList(jsonObj.getJSONArray("run_list").toString());
-				//nodePojo.setUptime(jsonObj.getJSONObject("automatic").getString("uptime"));
-				//nodePojo.setHostname(jsonObj.getJSONObject("automatic").getString("hostname"));
-				nodePojo.setFqdn(jsonObj.getJSONObject("automatic").getString("fqdn"));
-				nodePojo.setOs_version(jsonObj.getJSONObject("automatic").getString("os_version"));
-				nodePojo.setPlatform_version(jsonObj.getJSONObject("automatic").getString("platform_version"));
-				nodePojo.setCpu_total(jsonObj.getJSONObject("automatic").getJSONObject("cpu").getInt("total"));
-				nodePojo.setPlatform(jsonObj.getJSONObject("automatic").getString("platform"));
-				nodePojo.setOs(jsonObj.getJSONObject("automatic").getString("os"));
-				nodePojo.setIpaddress(jsonObj.getJSONObject("automatic").getString("ipaddress"));
-				nodePojo.setRecipes(jsonObj.getJSONObject("automatic").getJSONArray("recipes").toString());
-
+				
+				if(jsonObj.getJSONObject("automatic") != null && jsonObj.getJSONObject("automatic").length() > 0) {
+					if(jsonObj.getJSONObject("automatic").getString("uptime") != null) {
+						nodePojo.setUptime(jsonObj.getJSONObject("automatic").getString("uptime"));
+					}
+					else nodePojo.setUptime("");
+					
+					nodePojo.setHostname(jsonObj.getJSONObject("automatic").getString("hostname"));
+					nodePojo.setFqdn(jsonObj.getJSONObject("automatic").getString("fqdn"));
+					nodePojo.setOs_version(jsonObj.getJSONObject("automatic").getString("os_version"));
+					nodePojo.setPlatform_version(jsonObj.getJSONObject("automatic").getString("platform_version"));
+					nodePojo.setCpu_total(jsonObj.getJSONObject("automatic").getJSONObject("cpu").getInt("total"));
+					nodePojo.setPlatform(jsonObj.getJSONObject("automatic").getString("platform"));
+					nodePojo.setOs(jsonObj.getJSONObject("automatic").getString("os"));
+					nodePojo.setIpaddress(jsonObj.getJSONObject("automatic").getString("ipaddress"));
+					nodePojo.setRecipes(jsonObj.getJSONObject("automatic").getJSONArray("recipes").toString());
+				}
+				else {
+					nodePojo.setUptime("Node bootstrapped yet");
+					nodePojo.setHostname("Node bootstrapped yet");
+					nodePojo.setFqdn("Node bootstrapped yet");
+					nodePojo.setOs_version("Node bootstrapped yet");
+					nodePojo.setPlatform_version("Node bootstrapped yet");
+					nodePojo.setCpu_total(0);
+					nodePojo.setPlatform("Node bootstrapped yet");
+					nodePojo.setOs("Node bootstrapped yet");
+					nodePojo.setIpaddress("Node bootstrapped yet");
+					nodePojo.setRecipes("Node bootstrapped yet");
+					
+				}
 				jsonObjForUI = new JSONObject(objMapper.writer().withDefaultPrettyPrinter().writeValueAsString(nodePojo));
 				jsonArrayNodes.put(jsonObjForUI);
 			    
@@ -98,33 +208,18 @@ public class NodeListServlet extends HttpServlet {
 	private void returnJSONArrayOfNodes(HttpServletRequest request, HttpServletResponse response, JSONArray jsonArrayNodes) 
 			throws ServletException, IOException {
 		
-		request.setCharacterEncoding("utf8");
+		response.setCharacterEncoding("utf8");
 		response.setContentType("application/json");
 		PrintWriter out = response.getWriter();
 		JSONObject obj = new JSONObject();
 		try {
 			System.out.println("jsonArrayNodes ::: "+jsonArrayNodes);
-			obj.put("message", jsonArrayNodes);
+			obj.put("jsonArrayNodes", jsonArrayNodes);
 		} 
 		catch (Exception e) {
 			e.printStackTrace();
 		}
 		out.print(obj);
-	}
-	
-	
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		try {
-			JSONArray jsonArrayNodes = geNodeList();
-			returnJSONArrayOfNodes(request, response, jsonArrayNodes);
-		}
-		catch(Exception exp) {
-			exp.printStackTrace();
-		}
 	}
 	
 }
